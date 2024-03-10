93c53d3ac330f009cd2f8dc8acec7146a5bdd3bf
Jiri Konvicny <konviji1@fit.cvut.cz>
2024-03-10 11:48:40 +0100

    Add Scraper project

---
+++ scraper/src/main/kotlin/cz/phishingalert/scraper/ScraperApplication.kt
@@ -0,0 +1,11 @@
+package cz.phishingalert.scraper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScraperApplication
+
+fun main(args: Array<String>) {
+    runApplication<ScraperApplication>(*args)
+}
