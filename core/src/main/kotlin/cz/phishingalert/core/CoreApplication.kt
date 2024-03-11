35b1bc39a806c8c65d1810527c73cee4cdf01d3e
Jiri Konvicny <konviji1@fit.cvut.cz>
2024-03-11 15:06:23 +0100

    Implement small sample of MQ communication

--- core/src/main/kotlin/cz/phishingalert/core/CoreApplication.kt
+++ core/src/main/kotlin/cz/phishingalert/core/CoreApplication.kt
@@ -1,10 +1,28 @@
 package cz.phishingalert.core
 
+import org.springframework.amqp.core.Queue
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+
+
 
 @SpringBootApplication
-class CoreApplication
+class CoreApplication {
+	@Bean
+	fun myQueue(): Queue {
+		return Queue("myQueue", false)
+	}
+
+	@Bean
+	fun runner(template: RabbitTemplate): ApplicationRunner {
+		return ApplicationRunner {
+			template.convertAndSend("myQueue", "Hello there :)")
+		}
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<CoreApplication>(*args)
--- scraper/src/main/kotlin/cz/phishingalert/scraper/ScraperApplication.kt
+++ scraper/src/main/kotlin/cz/phishingalert/scraper/ScraperApplication.kt
@@ -1,10 +1,17 @@
 package cz.phishingalert.scraper
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+
 @SpringBootApplication
-class ScraperApplication
+class ScraperApplication {
+    @RabbitListener(queues = ["myQueue"])
+    fun listen(msg: String) {
+        println("Message read from myQueue : $msg")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<ScraperApplication>(*args)
