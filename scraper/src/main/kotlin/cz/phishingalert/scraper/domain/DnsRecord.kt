2fd38c17113d1728a7d1674d3434382b0baa30e4
Jiri Konvicny <konviji1@fit.cvut.cz>
2024-03-22 15:14:31 +0100

    Update DnsRecord properties

--- scraper/src/main/kotlin/cz/phishingalert/scraper/domain/DnsRecord.kt
+++ scraper/src/main/kotlin/cz/phishingalert/scraper/domain/DnsRecord.kt
@@ -3,9 +3,9 @@ package cz.phishingalert.scraper.domain
 data class DnsRecord(
     override var id: Int?,
     var name: String,
-    var type: String,
+    var type: Int,  //type id (decimal)
     var ipAddress: String,
-    var timeToLive: Int?,
-    var priority: Int?
+    var timeToLive: Long?,
+    var priority: Int? = 0
 ) : Entity<Int> {
 }
