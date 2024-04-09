5fe922e9f597416b5ee34de674a42368beaa1673
Jiri Konvicny <konviji1@fit.cvut.cz>
2024-04-09 20:25:38 +0200

    Add toLocalDateTime function

--- scraper/src/main/kotlin/cz/phishingalert/scraper/utils/DateParser.kt
+++ scraper/src/main/kotlin/cz/phishingalert/scraper/utils/DateParser.kt
@@ -2,8 +2,10 @@ package cz.phishingalert.scraper.utils
 import java.time.DateTimeException
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeFormatterBuilder
+import java.util.*
 
 object DateParser {
     private val formatter = DateTimeFormatter.ofPattern(
@@ -28,4 +30,12 @@ object DateParser {
             LocalDate.of(1970, 1, 1)   // 00:00:00 1 January 1970
         }
     }
+
+    /**
+     * Convert java.util.Date to java.time.LocalDateTime
+     * Method from https://www.baeldung.com/java-date-to-localdate-and-localdatetime
+     * @return LocalDateTime object equivalent to passed Date
+     */
+    fun toLocalDateTime(date: Date): LocalDateTime = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+
 }
